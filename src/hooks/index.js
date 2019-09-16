1b00d68209e2e82d4d516c7ed90e1962bc5f9767
Tanisha <tanisha031199@gmail.com>
2019-09-17 00:22:11 +0530

    added

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { firebase } from "../firebase";
-import {collatedTasksExist} from '../helpers'
-import moment from 'moment'
+import { collatedTasksExist } from "../helpers";
+import moment from "moment";
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
+
   useEffect(() => {
     let unsubscribe = firebase
       .firestore()
@@ -22,5 +24,22 @@ export const useTasks = selectedProject => {
         : selectedProject === "INBOX" || selectedProject === 0
         ? (unsubscribe = unsubscribe.where("date", "==", ""))
         : unsubscribe;
-  }, []);
+
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: task.id,
+        ...task.data()
+      }));
+      setTasks(
+        selectedProject === "NEXT_7"
+          ? newTasks.filter(
+              task =>
+                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      );
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
+    });
+  }, [selectedProject]);
 };
