60f2a607d7c489359ca72a9f4b7ef75ab610791b
Tanisha <tanisha031199@gmail.com>
2019-09-19 00:14:59 +0530

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,27 @@ import { FaPizzaSlice } from "react-icons/fa";
 
 export function Header() {
   return (
-    <div>
-      <h1>TODO-ist</h1>
+    <header className="header">
+      {/* <h1>TODO-ist</h1> */}
       <nav>
         <div className="logo">
+        <span style={{color:'#ffffff',fontSize:'30px', marginRight:'20px', paddingBottom:'20px'}}> TODO-ist</span>
           <img
             src="https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-12-26/17403075280_024490441c688e6ab5f8_512.png"
             alt="Todoist"
-            height="100"
-            width="100"
+            height="30"
+            width="35"
           />
         </div>
         <div className="settings">
           <ul>
-            <li>+</li>
-            <li>
+            <li className="settings__add">+</li>
+            <li className="settings__darkmode">
               <FaPizzaSlice />
             </li>
           </ul>
         </div>
       </nav>
-    </div>
+    </header>
   );
 }
